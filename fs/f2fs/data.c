efcbc9022cb54a844bab833b4d317fc8843c4196
Chao Yu <yuchao0@huawei.com>
2021-05-10 17:30:32 +0800

    f2fs: compress: fix to assign cc.cluster_idx correctly
    In f2fs_destroy_compress_ctx(), after f2fs_destroy_compress_ctx(),
    cc.cluster_idx will be cleared w/ NULL_CLUSTER, f2fs_cluster_blocks()
    may check wrong cluster metadata, fix it.
    
    Fixes: 4c8ff7095bef ("f2fs: support data compression")
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -2346,7 +2346,7 @@ int f2fs_mpage_readpages(struct address_space *mapping,
 							max_nr_pages,
 							&last_block_in_bio,
 							is_readahead, false);
-				f2fs_destroy_compress_ctx(&cc);
+				f2fs_destroy_compress_ctx(&cc, false);
 				if (ret)
 					goto set_error_page;
 			}
@@ -2389,7 +2389,7 @@ next_page:
 							max_nr_pages,
 							&last_block_in_bio,
 							is_readahead, false);
-				f2fs_destroy_compress_ctx(&cc);
+				f2fs_destroy_compress_ctx(&cc, false);
 			}
 		}
 #endif
@@ -3106,7 +3106,7 @@ next:
 		}
 	}
 	if (f2fs_compressed_file(inode))
-		f2fs_destroy_compress_ctx(&cc);
+		f2fs_destroy_compress_ctx(&cc, false);
 #endif
 	if (retry) {
 		index = 0;
